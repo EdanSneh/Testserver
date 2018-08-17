66c587b67efd9752c55943d5bf13cea39f1f1f0a
Edan Sneh <edan.sneh@gmail.com>
2018-08-16 19:15:24 -0700

    commit 3

--- first-site/src/index.js
+++ first-site/src/index.js
@@ -57,32 +57,25 @@ class Board extends React.Component {
   }
 
   render() {
-    let gameboard;
-    gameboard.map((x) => {
-      return(<div>);
-    });
-    for(let i = 0; i<3; i++){
-      return (
-        <div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
         </div>
-      );
-    }
-    
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
   }
 }
 
