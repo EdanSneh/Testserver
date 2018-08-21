d520533e6a782ab9d4475a492433950fc4cc5b56
Edan Sneh <edan.sneh@gmail.com>
2018-08-21 15:40:27 -0700

    made my own component

---
+++ notes/notes6.md
@@ -0,0 +1,16 @@
+# Component Lifecycle
+
+1. Component doesnt exist
+2. mount component
+3. existing
+4. unmount component
+5. disapearing
+
+## Mounting stage
+Methods called when method inserted into DOM:
+- constructor()
+- getDervedStateFromProps()
+- render()
+- componentDidMount()
+- ~~Depricated: componentWillMount()~~
+
---
+++ third-site/confusion/src/components/DishdetailComponent.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+
+class DishDetail extends Component {
+    
+    constructor(props){
+        super(props);
+    }
+
+    
+    renderDish(dish) {
+        if(dish != null){
+            return(
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} /> 
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+        else {
+            return(
+                <div></div>
+            )
+        }
+    }
+    renderDish(dish){
+        return (
+            <div className="col=12 col-md-5 m-1">
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} /> 
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        );
+    }
+
+    renderComments(comments){
+        const commentlist = comments.map((comment) => {
+            return(
+                <div key={comment.id}>
+                    <p>{comment.comment}</p>
+                    <p>--{comment.author}, {comment.date}</p>
+                </div>
+            )
+        });
+        
+        return(commentlist)
+
+    }
+
+    render(){
+
+        if(this.props.dish != null){
+            return(
+                <div className="row">
+                    {this.renderDish(this.props.dish)}
+                    <div className="col=12 col-md-5 m-1">
+                        <h4>Comments</h4>
+                        {this.renderComments(this.props.dish.comments)}
+                    </div>
+                </div>
+            );
+        }
+        else{
+            return(<div></div>)
+        }
+    }
+    
+
+}
+
+export default DishDetail;
